f1afb4bf9f642688b1ebead2eddaab43f5a3418a
Aman Kumar <heavengamerz109@gmail.com>
2025-06-17 14:30:06 +0530

    list comprehension

--- learn.py
+++ learn.py
@@ -7,9 +7,27 @@
 # print(res)
 
 
-n=int(input("Enter size of the list: "))
-l=[]
-for i in range(n):
-    l.append(i)
-print(l)
+# n=int(input("Enter size of the list: "))
+# l=[]
+# for i in range(n):
+#     l.append(i)
+# print(l)
 
+mat=[[1,2,3],
+     [4,5,6]]
+
+row=len(mat)
+col=len(mat[0])
+
+
+# for i in range(col):
+#     m=[]
+#     for j in range(row):
+#         m.append(mat[j][i])
+#     t.append(m)
+# print(t)
+ 
+#using list comprehension
+
+transpose = [[mat[j][i] for j in range(row)] for i in range(col)]
+print(transpose)
