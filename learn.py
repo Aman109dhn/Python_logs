fabb46c78160aa7b94c0af44f0358f58bff14000
Aman Kumar <heavengamerz109@gmail.com>
2025-06-17 14:01:10 +0530

    Decorators

---
+++ learn.py
@@ -0,0 +1,15 @@
+#map,filter and reduce
+
+# a=[1,2,3,4]
+# def val(n):
+#     return n**2
+# res=list(map(val,a))  #map(function,iterable)
+# print(res)
+
+
+n=int(input("Enter size of the list: "))
+l=[]
+for i in range(n):
+    l.append(i)
+print(l)
+
